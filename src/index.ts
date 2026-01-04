11bc88e3c7e5a4ffedf9a79cc8b96a0a9a08d061
Dusan Navidu <navidu200210@gmail.com>
2026-01-05 00:55:55 +0530

    url

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,52 @@
 // src/index.ts
 
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import authRouter from "./routes/auth";
-import organizerRouter from "./routes/organizer";
-import eventRouter from "./routes/event";
-import playerRouter from "./routes/playerDetails";
-import post from "./routes/post";
-import applicationRouter from "./routes/application";
-import aiRouter from "./routes/ai";
-import { authenticate } from "./middleware/auth";
-import { requireRole } from "./middleware/role";
-import { Role } from "./models/user.model";
-import { createDefaultAdmin } from "./utils/createDefaultAdmin";
-dotenv.config();
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import authRouter from "./routes/auth"
+import organizerRouter from "./routes/organizer"
+import eventRouter from "./routes/event"
+import playerRouter from "./routes/playerDetails"
+import post from "./routes/post"
+import applicationRouter from "./routes/application"
+import aiRouter from "./routes/ai"
+import { authenticate } from "./middleware/auth"
+import { requireRole } from "./middleware/role"
+import { Role } from "./models/user.model"
+import { createDefaultAdmin } from "./utils/createDefaultAdmin"
+dotenv.config()
 
-const app = express();
+const app = express()
 
-const PORT = process.env.PORT;
-const MONGO_URI = process.env.MONGO_URI as string;
+const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGO_URI as string
 
-app.use(express.json());
-
+app.use(express.json())
 app.use(
-  cors({
-    origin: [
-      "https://my-team-front-end-mu.vercel.app",
-      "http://localhost:5173",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  })
-);
+    cors({
+        origin: ["http://localhost:5173","http://localhost:5174","https://rad-72-sample-fe.vercel.app/login"],
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    })
+)
 
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/ai", aiRouter);
-app.use("/api/v1/organizer", organizerRouter);
-app.use("/api/v1/event", eventRouter);
-app.use("/api/v1/player", playerRouter);
-app.use("/api/v1/post", post);
-app.use("/api/v1/applications", applicationRouter);
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/ai", aiRouter)
+app.use("/api/v1/organizer", organizerRouter)
+app.use("/api/v1/event", eventRouter)
+app.use("/api/v1/player", playerRouter)
+app.use("/api/v1/post", post)
+app.use("/api/v1/applications", applicationRouter)
 
 app.get("/", (req, res) => {
-  res.send("Backend is running...");
-});
+    res.send("Backend is running...")
+})
 
-app.get("/test-1", (req, res) => {});
+app.get("/test-1", (req, res) => {})
 
-app.get("/test-2", authenticate, (req, res) => {});
+app.get("/test-2", authenticate, (req, res) => {})
 
-app.get("/test-3", authenticate, requireRole([Role.ADMIN]), (req, res) => {});
+app.get("/test-3", authenticate, requireRole([Role.ADMIN]), (req, res) => {})
 
 const startServer = async () => {
   try {
