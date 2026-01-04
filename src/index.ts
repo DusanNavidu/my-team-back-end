6042da6d8b16c5283cb4f9fce3cd4ce4c8c8f580
Dusan Navidu <navidu200210@gmail.com>
2026-01-04 15:18:11 +0530

    f

--- src/index.ts
+++ src/index.ts
@@ -24,10 +24,7 @@ const MONGO_URI = process.env.MONGO_URI as string;
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://my-team-front-end-mu.vercel.app/",
-    ],
+    origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
